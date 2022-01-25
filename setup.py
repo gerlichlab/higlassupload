1b7229789cf75f6b498c55f9a0ce333ee429996b
Mittmich <michael_mitter@hotmail.com>
2022-01-25 11:38:56 +0100

    added sacCer3 to assets

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ setup(
     name='higlassUp',
     version='0.1',
     py_modules=['higlassUp'],
-    data_files=[("./", ["hg19.chrom.sizes", "hg38.chrom.sizes"])],
+    data_files=[("./", ["hg19.chrom.sizes", "hg38.chrom.sizes", "mm9.chrom.sizes", "sacCer3.chrom.sizes"])],
     install_requires=[
         'Click',
         'clodius'
